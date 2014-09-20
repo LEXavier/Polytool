b4d1f140ef71d2eb4f81852d4ce07883089e2bfa
Luiz Eduardo <lesxavier@outlook.com>
2014-09-20 09:13:12 -0300

    Polybox v1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+Primeiro trabalho de Computação I da Turma 2014.2 - UFRJ
+Autores: Luiz Eduardo Xavier & Rodrigo Paiva
+Arquivos:
+- polytool.c
+- polybox.h
+- polybox.c
+
--- polybox.c
+++ polybox.c
@@ -6,7 +6,7 @@ float peval(Polynome p , float x){
 /*
 	Descrição: retorna o valor de p(x)
 	Entrada: O polinômio p, e o valor de x;
-	Saída: p(x), que também será impresso na saída padrão;
+	Saída: p(x);
 	Autor: Luiz Eduardo Xavier
 	Data: 15/09/2014
 */
@@ -19,6 +19,14 @@ float peval(Polynome p , float x){
 }
 
 void printP(Polynome p){
+/*
+	Descrição: imprime p(x) na saída padrão;
+	Entrada: O polinômio p;
+	Saída: a impressão de p(x);
+	Autor: Luiz Eduardo Xavier
+	Data: 15/09/2014
+*/
+	printf("p(x) = ");
 	if (p.a9 != 0) printf("%.2fx^9 ",p.a9);
 	if (p.a8 < 0) printf("%.2fx^8 ",p.a8)
 	else printf("+%.2fx^8 ",p.a8);
@@ -40,3 +48,83 @@ void printP(Polynome p){
 	else printf("+%.2f\n",p.a0);
 }
 
+float calculaDelta(float a , float b , float c){
+	return (b*b -4*a*c);
+}
+
+void roots(Polynome p){
+/*
+	Descrição: Calcula as raízes de um polinômio;
+	Entrada: O polinômio p(x);
+	Saída: A impressão da(s) raíze(s) na saída padrão;
+	Autor: Luiz Eduardo Xavier;
+	Data: 20/09/2014;
+*/
+	float root,root1;
+	float delta;
+	if (p.grau == 1) {
+		if (p.a1 != 0){
+			root = -(p.a0/p.a1);
+			printf(":/> %.2f\n",root);
+		}
+		else puts("Raiz indeterminada");
+		return;
+	}
+	if (p.grau == 2)
+		delta = calculaDelta(p.a2,p.a1,p.a0);
+		if (delta >= 0){
+			root = (sqrt(delta)-p.a1)/(2*p.a2);
+			root1 = -(sqrt(delta)+p.a1)/(2*p.a2);
+			printf(":/> {%.2f,%.2f}\n",root,root1);
+		}
+		else puts("Não possui raízes reais");
+		return;
+	} else 
+		puts("O grau do polinômio deve ser inferior a (1 ou 2)");
+}
+
+void optionMenu(Polynome p){
+/*
+	Descrição: Menu de opções para trabalhar com um polinômio;
+	Entrada: O polinômio p que será trabalhado;
+	Saída: ---
+	Autor: Luiz Eduardo Xavier;
+	Data: 20/09/2014;
+*/
+	int resp = 0;
+	while (resp != 0){
+		printf("O que deseja fazer agora?\n");
+		printf("(1) Mostra polinômio;\n");
+		printf("(2) Modificar polinômio;\n");
+		printf("(3) Calcular p(x);\n");
+		printf("(4) Calcular raízes (o grau deve ser menor ou igual a 2);\n");
+		printf("(5) Derivar polinômio;\n");
+		printf("(6) Somar com outro polinômio;\n");
+		printf("(7) Sair\n"); 
+		printf(":/>");
+		scanf("%d",&resp);
+		switch (resp) {
+			case 1:
+				printP(p);
+			break;
+			case 2:
+				modPoly(p);
+			break;
+			case 3:
+				peval(p);
+			break;
+			case 4:
+				roots(p);
+			break;
+			case 5:
+				diff(p);
+			break;
+			case 6:
+				sumP(p);
+			break;
+			default:
+				if (resp != 7) printf("Opção inválida!");
+			break;
+		}
+	}
+}
