902f3a644849768e0b633b0c6d17e885f54aba86
LEXavier <lesxavier@outlook.com>
2014-09-23 12:15:22 -0300

    Adicionada nova type (Coeficiente)

--- polybox.h
+++ polybox.h
@@ -3,7 +3,13 @@
 */
 
 typedef struct {
-	float a9,a8,a7,a6,a5,a4,a3,a2,a1,a0; //coeficientes do polinômio
+	char sinal;
+	float val;
+	int grau;
+} Coeficiente;
+
+typedef struct {
+	Coeficiente a9,a8,a7,a6,a5,a4,a3,a2,a1,a0; //coeficientes do polinômio
 	int grau;
 } Polynome;
 
